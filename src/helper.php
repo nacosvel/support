db9aec66ae5dee2e7070a2398e0b082e79529716
jundayw <jundayw@126.com>
2025-12-06 14:20:22 +0800

    function_exists('with')

--- src/helper.php
+++ src/helper.php
@@ -69,3 +69,21 @@ if (!function_exists('value')) {
         return $value instanceof Closure ? $value(...$args) : $value;
     }
 }
+
+if (!function_exists('with')) {
+    /**
+     * Return the given value, optionally passed through the given callback.
+     *
+     * @template TValue
+     * @template TReturn
+     *
+     * @param TValue                             $value
+     * @param (callable(TValue): (TReturn))|null $callback
+     *
+     * @return ($callback is null ? TValue : TReturn)
+     */
+    function with($value, ?callable $callback = null)
+    {
+        return is_null($callback) ? $value : $callback($value);
+    }
+}
