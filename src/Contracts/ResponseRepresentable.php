38d56233e6c94ff2f9f0f3d907c8c5c6c79d53bb
jundayw <jundayw@126.com>
2025-12-18 13:59:24 +0800

    ResponseRepresentable

---
+++ src/Contracts/ResponseRepresentable.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Nacosvel\Support\Contracts;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Represents an object that can be converted into an HTTP response.
+ *
+ * This interface is useful for objects that need to return a full HTTP response
+ * based on the given request context, such as controllers, API resources, or view models.
+ */
+interface ResponseRepresentable
+{
+    /**
+     * Convert the object into an HTTP response.
+     *
+     * @param Request|null $request The current HTTP request context.
+     *
+     * @return Response The corresponding HTTP response object.
+     */
+    public function toResponse(?Request $request = null): Response;
+}
